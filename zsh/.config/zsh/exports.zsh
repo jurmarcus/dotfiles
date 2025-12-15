16525a16e2e9dddafe53c2cc3cdd08ef3175b231
Jurmarcus Allen <me@jurmarcus.com>
2025-12-15 12:00:21 +0900

    ghostty things

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -10,3 +10,7 @@ SAVEHIST=100000
 export FZF_DEFAULT_COMMAND='fd --hidden --follow --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
+
+# Editor configuration
+export EDITOR="codium --wait"
+export VISUAL="codium --wait"
